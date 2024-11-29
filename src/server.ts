39f4c3fea5a255d7e7507b5b4e01ea8d94b200c6
Cherno <daniel.cherno9@gmail.com>
2024-11-29 12:37:58 +0200

    added basic server.ts functionality

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,26 @@ import express from 'express';
 import mongoose from "mongoose";
 import cookieParser from 'cookie-parser'; 
 import 'dotenv/config';
+const app = express()
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+
+const dbUrl = process.env.DB_URL;
+const database = 'bobbleheads';
+
+mongoose.connect(`${dbUrl}/${database}`).then(()=>{
+    console.info("DB connected")
+}).catch((err)=>{
+    console.error(err)
+});
+
+//routes
+/*import xRouter from './routes/x/xRoutes';
+app.use("/api/x", xRouter);*/
+
+app.listen(port, () => {
+    console.log(`BubbleHead server is up!`)
+  })
